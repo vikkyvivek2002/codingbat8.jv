1447d27e6580c7cf7f5a8fb85f208ca5e5d0f612
vivek vardhan . M <84318895+vikkyvivek2002@users.noreply.github.com>
2021-11-16 22:57:44 +0530

    Add files via upload

---
+++ codingbat8.java
@@ -0,0 +1,13 @@
+public boolean nearHundred(int n) {
+  boolean x = true;
+  if(n>=90 && n<=110){
+     x = true;
+  }
+  else if(n>=190  && n<=210){
+     x= true;
+  }
+  else{
+     x= false ;
+  }
+  return x;
+}
